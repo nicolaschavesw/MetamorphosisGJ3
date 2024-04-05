4643b576b7ff787a61c0074e5ca088ef6fce0046
Fernanda Romero <aidafernanda1990@hotmail.com>
2024-04-04 21:41:03 -0500

    Enemy Prefab

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,10 +6,13 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     public Transform player; 
-    public float minDistance = 5f;
+    private float minDistance = 5f;
     private NavMeshAgent agent; 
     private Animator animator;
     private bool isWalking = false;
+    private bool isInRange = false;
+    private float detectionAngle = 60f;
+    private float detectionRange = 10f;
 
     void Start()
     {
@@ -21,7 +24,7 @@ public class EnemyController : MonoBehaviour
     {
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        if(distanceToPlayer > minDistance)
+        if(distanceToPlayer < minDistance)
         {
           agent.SetDestination(player.position);  
         }
@@ -33,21 +36,48 @@ public class EnemyController : MonoBehaviour
     {
         if (agent.velocity.magnitude > 0.1f)
         {
-            // Cambiar a la animación de caminar si el agente está caminando
             if (!isWalking)
             {
                 animator.SetBool("isWalking", true);
+                animator.SetBool("isReacting", false);
                 isWalking = true;
             }
         }
         else
         {
-            // Cambiar a la animación de inactividad si el agente no está caminando
             if (isWalking)
             {
                 animator.SetBool("isWalking", false);
+                animator.SetBool("isReacting", false);
                 isWalking = false;
             }
         }
+
+
+        Vector3 directionToPlayer = player.position - transform.position;
+        float angle = Vector3.Angle(transform.forward, directionToPlayer);
+        float distanceToPlayer = directionToPlayer.magnitude;
+
+        if (angle < detectionAngle / 2 && distanceToPlayer < detectionRange)
+        {
+            isInRange = true;
+            animator.SetBool("isReacting", true);
+        }
+        else
+        {
+            isInRange = false;
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+
+        Vector3 rightLimit = Quaternion.Euler(0, detectionAngle / 3, 0) * transform.forward;
+        Gizmos.DrawLine(transform.position, transform.position + rightLimit * detectionRange);
+
+        Vector3 leftLimit = Quaternion.Euler(0, -detectionAngle / 3, 0) * transform.forward;
+        Gizmos.DrawLine(transform.position, transform.position + leftLimit * detectionRange);
     }
 }
+
